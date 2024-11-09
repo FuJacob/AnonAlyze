7ea2ba15347a7c3a452b95bc2eed23844fcd41c9
Ahmed <ahmedabduljader@trentu.ca>
2024-11-09 16:45:36 -0500

    removed font and improted into about

--- frontend/src/components/about.jsx
+++ frontend/src/components/about.jsx
@@ -1,4 +1,11 @@
 import React from 'react'
+import { VT323 } from 'next/font/google';
+
+const vt323 = VT323({
+    weight: '400',
+    subsets: ['latin'],
+    display: 'swap',
+  });
 
 const about = () => {
   return (
