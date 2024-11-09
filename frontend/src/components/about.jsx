a18fb4405a38c7e1b4b3c8aed89f3fd3414fb838
Jacob Fu <jjacobfu@gmail.com>
2024-11-09 17:14:07 -0500

    asd

--- frontend/src/components/about.jsx
+++ frontend/src/components/about.jsx
@@ -1,9 +1,14 @@
-import React from 'react'
+import React from 'react';
 
-const about = () => {
+const About = () => {
   return (
-    <div>about</div>
-  )
+    <div className="grid grid-cols-4 gap-4">
+      <img src="/images/jacob.png" alt="Jacob" className="w-1/2 h-1/2"/>
+      <img src="/images/ahmed.png" alt="Ahmed" className="w-1/2 h-1/2"/>
+      <img src="/images/andy.png" alt="Andy" className="w-1/2 h-1/2"/>
+      <img src="/images/jahien.png" alt="Jahien" />
+    </div>
+  );
 }
 
-export default about
+export default About;
