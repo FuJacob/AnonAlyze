96d3a234f264db617e69503ea8fcad0a144952f9
Ahmed <ahmedjader837@gmail.com>
2024-11-10 00:33:43 -0500

    updated about section name information of group, population openai rating json info

--- frontend/src/components/hero.jsx
+++ frontend/src/components/hero.jsx
@@ -21,6 +21,16 @@ const profileData = {
     profileUrl: "https://www.instagram.com/pakmangames/"
 };
 
+const openRating = {
+    username: "pakmangames",
+    fullName: "Andy",
+    biography: "mac swe\n⋆･ ｡ﾟ *.* ☆ﾟ.☾ ⋆*･ﾟ:⋆*",
+    followersCount: 234,
+    followsCount: 363,
+    profilePicUrl: "https://scontent-scl2-1.cdninstagram.com/v/t51.2885-19/356235487_1434762333987767_4094130844684754685_n.jpg?stp=dst-jpg_e0_s150x150&_nc_ht=scontent-scl2-1.cdninstagram.com&_nc_cat=111&_nc_ohc=r0bhG7mvm6wQ7kNvgHeag9k&_nc_gid=46889b8d49474d29bee210694f42daf0&edm=AOQ1c0wBAAAA&ccb=7-5&oh=00_AYAKKyopUV-wF6-h25uq2PUimNH8pMZR6n10vywA3mS2pw&oe=673573C6&_nc_sid=8b3546",
+    profileUrl: "https://www.instagram.com/pakmangames/"
+};
+
 const Hero = () => {
     return (
         <section className="min-h-screen flex items-center justify-center text-white py-[7rem] mb-[-15rem]" id="hero">
@@ -30,11 +40,11 @@ const Hero = () => {
                     <p className="mb-10">Input your Instagram URL</p>
                     <input
                         type="text"
-                        className="text-black text-xl size-11 mb-10 p-6 w-80 h-24 border-2 border-gray-300 rounded text-center focus:ring-4 focus:ring-yellow-400"
+                        className="text-black text-xl size-11 mb-10 p-6 w-80 h-14 border-2 border-gray-300 rounded text-center focus:ring-4 focus:ring-yellow-400"
                         placeholder="Enter your Instagram Username" />
                     <div className="flex justify-center">
                         <button
-                            type="button" className="bg-white text-black border-2 border-gray-300 px-4 py-2 rounded shadow hover:bg-gray-100">
+                            type="button" className="bg-white text-black border-2 border-gray-300 px-4 py-2 h-14 rounded shadow hover:bg-gray-100">
                             AnonAlyze!
                         </button>
                     </div>
