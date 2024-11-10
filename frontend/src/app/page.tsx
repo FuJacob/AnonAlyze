755306ec5757da2fd12a7a755f4038683bd2f558
Jacob Fu <jacobfu84@gmail.com>
2024-11-09 23:05:13 -0500

    added header

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -2,10 +2,11 @@ import Navbar from "../components/navbar";
 import Hero from "../components/hero";
 import About from "../components/about";
 import Purpose from "../components/purpose";
-
+import Header from "../components/header";
 export default function Home() {
   return (
     <>
+    <Header />
       <Navbar />
       <main className="antialiased overflow-x-hidden max-w-7xl mx-auto relative z-10 animate-fadeIn2" id="Home">
         <Hero />
---
+++ frontend/src/components/header.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import { VT323 } from "next/font/google";
+
+const vt323 = VT323({
+  weight: "400",
+  subsets: ["latin"],
+  display: "swap",
+});
+
+const header = () => {
+  return (
+    <>
+      <section className="mt-16 px-6 py-10" id="about">
+        <div
+          className={`text-center text-neutral- ${vt323.className} text-3xl`}
+        >
+          <div className="flex items-center justify-center">
+            <img
+              src="/images/logo.png"
+              alt="Analyze Logo"
+              className="w-1/3"
+            />
+          </div>
+          <h1 className='text-5xl animate-pulse'>Unmasking Risks, Protecting Privacy</h1>
+        </div>
+      </section>
+    </>
+  );
+};
+
+export default header;
